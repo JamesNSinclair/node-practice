d0dccc51b1e0b137011667833bac9856198cc204
jamesnws <james@getinflow.io>
2023-04-07 12:17:21 +0100

    end of first module

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
  const http = require('http');
+ const fs = require('fs');
+ const routes = require('./routes');
 
- const server = http.createServer((req, res) => {
- console.log(req.url, req.method, req.headers);
- res.setHeader('Content-Type', 'text/html');
- res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-
- }
-);
+ const server = http.createServer(
+ routes.requestHandler(req, res)
+ );
 
 server.listen(3801)
